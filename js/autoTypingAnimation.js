f8f8cf07e0d309f423c82fecb1fe85e8a437506f
prati123456 <113717884+prati123456@users.noreply.github.com>
2024-02-13 20:09:19 +0530

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Pratiksha Satapure</span> , a dedicated web developer driven by a profound passion for software development and project management. With a background in Computer Engineering from Pimpri Chinchwad College of Engineering, Pune, I have honed my skills to merge theoretical knowledge with practical applications. My journey as a web developer is rooted in a fervent drive to innovate and create meaningful solutions. Embracing challenges with enthusiasm, I thrive on the dynamic intersection of technology and creativity. Beyond coding, I am passionate about software development methodologies and project management practices. Leveraging certifications in Agile Project Management and expertise in SQL databases, I bring a holistic approach to my projects, ensuring both technical excellence and efficient execution."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
